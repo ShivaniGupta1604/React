e1949aaca7c95fc31e79d11bbf37595098ed2ad7
sgupta5 <sgupta5@cvent.com>
2025-06-08 18:18:45 +0530

    React

---
+++ test.js
@@ -0,0 +1,63 @@
+// console.log("A Online Computer Science Portal\n for Geeks")
+
+// let gfg = 10, GFG = 5,
+//     Geeks =
+//         gfg + GFG;
+
+
+// console.log(Geeks)
+
+// let x = 5;
+
+// console.log(x);
+
+// console.log(y);
+
+// const obj1 = { a: 1 };
+// const obj2 = { a: 1 };
+// console.log(obj1 == obj2);
+// console.log(obj1 === obj2);
+
+
+// const a = 2;
+// const b = 2;
+// console.log(a == b);
+// console.log(a === b);
+
+
+// console.log('A');
+// setTimeout(() => console.log('B'), 0);
+// Promise.resolve().then(() => console.log('C'));
+// console.log('D');
+
+
+// let a = [1, 2, 3];
+// let b = a;
+// b.push(4);
+// console.log(a);
+// console.log(b);
+
+
+
+// 'use strict';
+
+// console.log(this)
+
+// function test() {
+//     console.log(this);
+// }
+// test.call(null);
+
+
+// console.log([1, 2] + [ 4,2]);
+
+// console.log([]);
+
+
+// (function () {
+//     var a = b = 5;
+//     console.log(typeof a);
+//     console.log(typeof b);
+// })();
+// console.log(typeof a);
+// console.log(typeof b);
